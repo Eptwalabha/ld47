469fa10cf5a81e64affb8e136868af077ff8488f
Eptwalabha <damiengabrielle@gmail.com>
2020-10-05 01:45:55 +0200

    add splash screen

---
+++ scenes/splash/SplashScreen.gd
@@ -0,0 +1,11 @@
+extends Control
+
+func _ready() -> void:
+	$AnimationPlayer.play("fade-in")
+
+func _input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed("ui_accept") or Input.is_action_just_pressed("ui_cancel"):
+		pass
+
+func _on_AnimationPlayer_animation_finished(anim_name: String) -> void:
+	get_tree().change_scene("res://scenes/Game.tscn")
