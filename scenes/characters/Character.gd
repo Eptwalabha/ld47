54d843b9601db7011aeea805026cc5eac429accd
Eptwalabha <damiengabrielle@gmail.com>
2020-10-05 18:19:40 +0200

    finish chapter 1

--- scenes/characters/Character.gd
+++ scenes/characters/Character.gd
@@ -1,7 +1,17 @@
+class_name Character
 extends Spatial
 
 export(String, "sit-drink", "sit-idle", "idle", "bartender", "danse", "sleep") var default := "sit-drink"
 
-
 func _ready() -> void:
-	$AnimationPlayer.play(default + "-loop")
+	play(default)
+
+func sit_drink() -> void: play("sit-drink")
+func sit_idle() -> void: play("sit-idle")
+func idle() -> void: play("idle")
+func danse() -> void: play("danse")
+func sleep() -> void: play("sleep")
+
+func play(animation: String) -> void:
+	animation = animation + ("" if animation.ends_with("-loop") else "-loop")
+	$AnimationPlayer.play(animation)
