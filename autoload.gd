74c7965d9830500bddcfb49c0d50446d23236c09
Eptwalabha <damiengabrielle@gmail.com>
2020-10-19 00:11:50 +0200

    fix missing audio bus effect

--- autoload.gd
+++ autoload.gd
@@ -1,5 +1,13 @@
 extends Node
 
+enum LEVEL {
+	HOME,
+	BAR,
+	ROAD,
+	HOSPITAL
+}
+
 var first_time : bool = true
 var using_controller : bool = false
 var debug : bool = false
+var first_level : int = LEVEL.BAR
