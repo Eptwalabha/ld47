18851840ef79335c8771366edcedd04cfde8bf0f
Eptwalabha <damiengabrielle@gmail.com>
2020-11-20 01:03:08 +0100

    change bar-v2 to bar
    change bar-v2 to bar

--- scenes/core/misc/MaterialPreloader.gd
+++ scenes/core/misc/MaterialPreloader.gd
@@ -1,37 +1,23 @@
 extends Spatial
 
-#var CeilingLight = preload('res://assets/models/bar-v2/ceiling-light.material')
-#var MirrorGlass = preload('res://assets/models/bar-v2/mirror-glass.material')
-#var ToiletSign = preload('res://assets/models/bar-v2/ToiletSign.material')
-#var BuildingCity = preload('res://assets/models/road/building-city.material')
-#var PhoneGlass = preload('res://assets/models/story/phone_glass.material')
-#
-#var materials := [
-#	CeilingLight,
-#	MirrorGlass,
-#	ToiletSign,
-#	BuildingCity,
-#	PhoneGlass,
-#]
-
 var materials := []
 var count = 0
 
 func _ready() -> void:
-	
+
 	var dir = Directory.new()
 	var path = "res://assets/models"
 	dir.open(path)
-	
+
 	_load_materials(dir)
-	
+
 	for material in materials:
 		var mesh = MeshInstance.new()
 		mesh.mesh = QuadMesh.new()
 		mesh.mesh.set_material(material)
 		mesh.visible = true
 		$Materials.add_child(mesh)
-	
+
 	set_process(true)
 
 func _load_materials(dir: Directory) -> void:
